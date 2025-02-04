81b92ab59ec130985e674b795fc9ef3edd16b034
lanaraEmili <44930349+lanaraEmili@users.noreply.github.com>
2025-02-03 21:21:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigo = [];
+let amigos = [];
 
 function adicionarAmigo() {
-    let nome = document.getElementById('amigo').value.trim();
+    let nome = document.querySelector('input').value;
+    
     //validar entrada vazia
     if (nome === '') {
-        alert('Por favor, insira um nome antes de adicionar');
+        alert('Por favor, insira um nome.');
         return;
     }
 
-    amigo.push(nome)
+    amigos.push(nome);
     nome.value = '';
+    limparCampo();
     criarLista();
 }
 
@@ -19,19 +21,24 @@ function criarLista() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
 
-    for (let i = 0; i < amigo.length; i++) {
+    for (let i = 0; i < amigos.length; i++ ) {
         let li = document.createElement('li');
-        li.textContent = amigo[i];
+        li.textContent = amigos[i];
         lista.appendChild(li);
     }
+}
+
+function limparCampo(){
+    nome = document.querySelector('input');
+    nome.value = '';
 }
 //função que sorteia o amigo e mostra no HTML para o usuário ver
 function sortearAmigo() {
     if (amigo.length == 0) {
         alert('Precisa colocar o nome de seus amigos para sortear o amigo secreto');
     } else {
-        let IndiceAleatorio = parseInt(Math.floor(Math.random() * amigo.length));
-        let nomeSorteado = amigo[IndiceAleatorio];
+        let IndiceAleatorio = parseInt(Math.floor(Math.random() * amigos.length));
+        let nomeSorteado = amigos[IndiceAleatorio];
         let amigoSecreto = document.getElementById('resultado');
         amigoSecreto.innerHTML = nomeSorteado;
     }
